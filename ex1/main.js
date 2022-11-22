ff2a2077ac8f2186338e110386c7e54d5f9dd5ca
FilipMilosevski <filipdh94@gmail.com>
2022-11-22 19:56:54 +0100

    new

---
+++ ex1/main.js
@@ -0,0 +1,15 @@
+let str1 = 'Laurence';
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let muNum = 1000; 
+
+
+
+
+
+console.log ("str1", typeof str1 === str2);
+console.log ("str2", typeof str2);
+console.log ("val1", typeof val1);
+console.log ("val2", typeof val2);
+console.log ("myNum", typeof myNum);
